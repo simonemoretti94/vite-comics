79a760073604fce79e250596fe9e149c1f0dda39
simonemoretti94 <simone.moretti.944@gmail.com>
2024-01-31 00:16:42 +0100

    2nd creating header

--- src/components/appheader.vue
+++ src/components/appheader.vue
@@ -11,26 +11,28 @@ export default {
 
 <template>
     <header>
-        <div class="container">
-            <div class="row">
-                <img class="logo" src="../assets/img/header&sub/sub_1/dc-logo.png" alt="dc-logo">
-                <ul>
-                    <a href="">characters</a>
-                    <a href="" class="a_blue">comics</a>
-                    <a href="">movies</a>
-                    <a href="">tv</a>
-                    <a href="">games</a>
-                    <a href="">collectibles</a>
-                    <a href="">videos</a>
-                    <a href="">fans</a>
-                    <a href="">news</a>
-                    <a href="">shop</a>
-                </ul>
-            </div>
+        <div class="container d-flex justify-content-between align-items-center">
+            <img class="logo" src="../assets/img/header&sub/sub_1/dc-logo.png" alt="dc-logo">
+            <ul>
+                <a href="">characters</a>
+                <a href="" class="a_blue">comics</a>
+                <a href="">movies</a>
+                <a href="">tv</a>
+                <a href="">games</a>
+                <a href="">collectibles</a>
+                <a href="">videos</a>
+                <a href="">fans</a>
+                <a href="">news</a>
+                <a href="">shop</a>
+            </ul>
         </div>
     </header>
 
-    <main>
+    <main class="d-flex justify-content-center">
+
+        <div class="container d-flex justify-content-start align-items-center">
+            <p>--&gt; Content goes here &lt;--</p>
+        </div>
 
     </main>
 </template>
@@ -41,41 +43,36 @@ header {
     min-height: 80px;
     background-color: var(--main-light);
     font-family: 'Open Sans', sans-serif;
+    display: flex;
+    justify-content: center;
+
+    & .container {
+
+        width: 80%;
+
+        & .logo {
+            width: 70px;
+            aspect-ratio: 1 / 1;
+        }
+    }
 }
 
 main {
-    min-height: 120px;
+    min-height: 80px;
     background-color: black;
+    color: var(--main-light);
     font-family: 'Open Sans', sans-serif;
-}
 
-.container {
-    width: 70%;
-    height: 80%;
-    max-width: 1176px;
-    margin: auto;
-}
+    & .container {
+        width: 80%;
 
-.row {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-
-
-
-}
-
-header .row {
-
-    padding-top: .3rem;
-
-    & .logo {
-        width: 70px;
-        aspect-ratio: 1 / 1;
+        & p {
+            font-weight: bolder;
+        }
     }
 }
+
+
 
 ul {
     display: flex;
