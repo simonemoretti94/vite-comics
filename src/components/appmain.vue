e77eda52d26fc32cfdb9fcef03065d6d1121dfcd
simonemoretti94 <simone.moretti.944@gmail.com>
2024-01-31 00:42:28 +0100

    4th working on appmain

--- src/components/appmain.vue
+++ src/components/appmain.vue
@@ -10,7 +10,39 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+</template>
 
 
-<style scoped></style>
+<style scoped>
+main {
+
+    min-height: 300px;
+    background-image: url('../assets/img/main&sub/jumbotron.jpg');
+    background-size: contain;
+    filter: grayscale(100%);
+
+    font-family: 'Open Sans', sans-serif;
+    display: flex;
+    justify-content: start;
+
+}
+
+footer {
+    min-height: 80px;
+    background-color: var(--main-dark);
+    font-family: 'Open Sans', sans-serif;
+    display: flex;
+    justify-content: center;
+
+}
+</style>
