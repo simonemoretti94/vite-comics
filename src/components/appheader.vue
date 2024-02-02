9f0ecb4b2551863dee685fc9725b64762adf8a3e
simonemoretti94 <simone.moretti.944@gmail.com>
2024-02-02 11:26:02 +0100

    14.2th added filter select

--- src/components/appheader.vue
+++ src/components/appheader.vue
@@ -16,72 +16,84 @@ export default {
                     "price": "$19.99",
                     "series": "Action Comics",
                     "type": "comic book",
+                    "tag": 'comic',
                 },
                 {
                     "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
                     "price": "$3.99",
                     "series": "American Vampire 1976",
                     "type": "comic book",
+                    "tag": 'drama',
                 },
                 {
                     "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
                     "price": "$16.99",
                     "series": "Aquaman",
                     "type": "graphic novel",
+                    "tag": 'novel',
                 },
                 {
                     "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
                     "price": "$2.99",
                     "series": "Batgirl",
                     "type": "comic book",
+                    "tag": 'drama',
                 },
                 {
                     "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
                     "price": "$3.99",
                     "series": "Batman",
                     "type": "comic book",
+                    "tag": 'comic',
                 },
                 {
                     "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
                     "price": "$2.99",
                     "series": "Batman Beyond",
                     "type": "comic book",
+                    "tag": 'novel',
                 },
                 {
                     "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
                     "price": "$3.99",
                     "series": "Batman/Superman",
                     "type": "comic book",
+                    "tag": 'drama',
                 },
                 {
                     "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
                     "price": "$4.99",
                     "series": "Batman/Superman Annual",
                     "type": "comic book",
+                    "tag": 'novel',
                 },
                 {
                     "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
                     "price": "$5.99",
                     "series": "Batman: The Joker War Zone",
                     "type": "comic book",
+                    "tag": 'comic',
                 },
                 {
                     "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
                     "price": "$6.99",
                     "series": "Batman: Three Jokers",
                     "type": "comic book",
+                    "tag": 'drama',
                 },
                 {
                     "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
                     "price": "$4.99",
                     "series": "Batman: White Knight Presents: Harley Quinn",
                     "type": "comic book",
+                    "tag": 'comic',
                 },
                 {
                     "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
                     "price": "$16.99",
                     "series": "Catwoman",
                     "type": "graphic novel",
+                    "tag": 'novel',
                 }
             ],
         }
@@ -108,10 +120,19 @@ export default {
         </div>
     </header>
 
-    <main class="d-flex justify-content-center">
+    <main class="d-flex flex-column justify-content-evenly align-items-center">
 
         <div class="container d-flex align-items-center flex-wrap">
             <appcards v-for="card in cardsArray" :card="card"></appcards>
+        </div>
+
+        <div id="div_filter" class="container col-5 d-flex justify-content-center my-2" >
+            <select name="filter" id="cards_filter">
+                <option value="">Tutti i tag</option>
+                <option value="comic">Comic</option>
+                <option value="drama">Drama</option>
+                <option value="novel">Novel</option>
+            </select>
         </div>
 
     </main>
@@ -209,6 +230,23 @@ main {
         & p {
             margin-top: 1rem;
             font-weight: bolder;
+        }
+    }
+
+    & div#div_filter {
+        & select {
+            background-color: var(--blue-comics);
+            padding: .2rem .3rem;
+            border-radius: 10px;
+
+            color: white;
+            text-shadow: .5px .5px black;
+
+            & option {
+                background-color: rgb(71, 186, 214);
+                color: white;
+                
+            }
         }
     }
 }
